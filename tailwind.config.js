aba10fbac1de77c2bf0b2ee0b0cc898620040f78
Kostas Kvietinskas <kostas.kvietinskas@gmail.com>
2022-12-21 13:22:41 +0000

    colours and html layout set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    colors: {
+      'bright-orange': 'hsl(31, 77%, 52%)',
+      'dark-cyan': 'hsl(184, 100%, 22%)',
+      'very-dark-cyan': 'hsl(179, 100%, 13%)',
+      'transparent-white': 'hsl(0, 0%, 100%, 0.75)',
+      'very-light-gray': 'hsl(0, 0%, 95%)',
+    }
   },
   plugins: [],
 }
